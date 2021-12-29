d95d06fda7b88556e364e10d94cc172c4e596f9c
Arkinmelv <80194106+Arkinmelv@users.noreply.github.com>
2021-12-29 13:08:27 +0700

    NavBar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h2>Hello World</h2>
+    <NavBar/>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from '../logo.png';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+
+const NavBar = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container">
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo.."></img></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <FontAwesomeIcon icon={faBars} style={{color: "black"}}/>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item active">
+            <a className="nav-link " aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+    )
+}
+
+export default NavBar
